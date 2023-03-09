e28f6eaf6cce513d45193a1e8a081f05a762827a
Yusif <yusif.feyzullazade@gmail.com>
2023-03-09 20:19:45 +0400

    new update

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,13 +4,14 @@ import { useSelector } from "react-redux";
 import HomeHeader from "../../components/HomeHeader/HomeHeader";
 import SearchMap from "../../components/SearchMap/SearchMap";
 import LeftBar from "../../components/LeftBar/LeftBar";
+import Apartment from "../../components/Apartments/Apartment";
+import HomeFooter from "../../components/HomeFooter/HomeFooter";
 
 interface AuthState {
   user: any | false;
 }
 
 const Home: React.FC = () => {
-
   const src =
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3039.404083412685!2d49.955428999999995!3d40.377736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1f580e9120fdbcb9!2zYXNhbiB4aWRtyZl0IMaPaG3JmWRsaQ!5e0!3m2!1saz!2s!4v1676406725236!5m2!1saz!2s";
   const width = 750;
@@ -20,16 +21,19 @@ const Home: React.FC = () => {
   const auth = useSelector((state) => state.auth);
   const user = auth.user as AuthState["user"] | false;
 
-
   if (user) {
     return (
-      <div className="home">
-        <LeftBar />
-        <div className="container">
-          <HomeHeader />
-          <SearchMap src={src} width={width} height={height} />
+      <>
+        <div className="home">
+          <LeftBar />
+          <div className="main-area">
+            <HomeHeader />
+            <SearchMap src={src} width={width} height={height} />
+            <Apartment />
+          </div>
         </div>
-      </div>
+        <HomeFooter />
+      </>
     );
   }
 
